20f67d04e5ff0659388e254f74c13cae01d42007
Abhishek <113371594+abhishek8396@users.noreply.github.com>
2023-08-05 13:27:16 +0530

    complete

---
+++ server/server.js
@@ -0,0 +1,40 @@
+import express from "express"
+
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./Connection/database.js"
+import morgan from "morgan"
+import authRoute from "./routes/authRoute.js"
+import categoryRoute from './routes/categoryRoute.js'
+import productRoute from './routes/productRoute.js'
+import path from "path"
+import {fileURLToPath} from 'url';
+const app= express(); 
+//configures env
+dotenv.config();
+// const PORT = process.env.PORT || 4500;
+
+//esmodule fix
+
+app.use(cors());
+//Middleware
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+//DataBase Connect
+connectDB();
+
+//routes
+app.use('/api/v1/auth', authRoute);
+//Rooutes for Category
+app.use('/api/v1/category', categoryRoute);
+//Routes for Product
+app.use('/api/v1/product', productRoute);
+
+
+const PORT = 8080;
+app.listen(PORT, ()=>{
+    console.log(`Server is running at https://localhost:${PORT}`.bgRed.white)
+})
+
